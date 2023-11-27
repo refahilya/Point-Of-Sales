f993cddf2535f0e2ac89e3e6910c2d3a60f3261e
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-11-27 22:17:55 +0700

    mung nyampah

--- PointOfSales/src/main/java/com/mycompany/pointofsales/PointOfSales.java
+++ PointOfSales/src/main/java/com/mycompany/pointofsales/PointOfSales.java
@@ -27,6 +27,24 @@ public class PointOfSales {
             
             ps.executeUpdate();
             */
+            
+            /*buat crud|create inventori
+            String query = "INSERT INTO inventori (id_barang, nama_barang, harga, stok, created_at) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)";
+            ps.setString(1, id_barang);
+            ps.setString(2, nama_barang);
+            ps.setInt(3, harga);
+            ps.setInt(4, stok);
+            */
+            
+            /*buat crud|create member
+            String query = "INSERT INTO member (id_member, nama_member, tanggal_lahir, no_telp, email, poin, created_at) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
+            ps.setString(1, id_member);
+            ps.setString(2, nama_member);
+            ps.setString(3, tanggal_lahir);
+            ps.setString(4, no_telp);
+            ps.setString(3, email);
+            ps.setInt(4, poin);
+            */
             konek.close();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());  
