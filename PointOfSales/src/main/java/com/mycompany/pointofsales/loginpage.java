8cbfe3a8af1629acc51b32b210d91def2a0442cc
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-12-01 10:17:59 +0700

    ck

--- PointOfSales/src/main/java/com/mycompany/pointofsales/loginpage.java
+++ PointOfSales/src/main/java/com/mycompany/pointofsales/loginpage.java
@@ -149,7 +149,7 @@ public class loginpage extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void loginpage(String args[]) {
+    public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
