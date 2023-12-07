141f82b45d409edabbd2018c8f5e2ea80f394f9a
RuthRadekta <ruthradekta@student.uns.ac.id>
2023-12-08 00:46:30 +0700

    riwayat page (update)
    ini mah sementara sek ya :bow:

--- PointOfSales/src/main/java/com/mycompany/pointofsales/riwayatpage.java
+++ PointOfSales/src/main/java/com/mycompany/pointofsales/riwayatpage.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.pointofsales;
 
+import java.awt.Color;
+
 /**
  *
  * @author ASUS
@@ -15,6 +17,7 @@ public class riwayatpage extends javax.swing.JFrame {
      */
     public riwayatpage() {
         initComponents();
+        getContentPane().setBackground(Color.decode("0xFFFFFF"));
     }
 
     /**
@@ -26,21 +29,159 @@ public class riwayatpage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        relasijy = new javax.swing.JLabel();
+        transaksi = new javax.swing.JButton();
+        riwayat = new javax.swing.JButton();
+        inventori = new javax.swing.JButton();
+        member = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        beranda = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1274, 698));
+        setMinimumSize(new java.awt.Dimension(1274, 698));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "ID BARANG", "ID TRANSAKSI", "NAMA BARANG", "JUMLAH", "HARGA SATUAN", "DISKON", "TOTAL HARGA"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 1240, 630));
+
+        relasijy.setIcon(new javax.swing.ImageIcon("D:\\Netbeans\\Point-Of-Sales\\PointOfSales\\src\\main\\java\\resources\\relasijy3.png")); // NOI18N
+        getContentPane().add(relasijy, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, -1));
+
+        transaksi.setBackground(new java.awt.Color(228, 228, 223));
+        transaksi.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        transaksi.setForeground(new java.awt.Color(0, 66, 37));
+        transaksi.setText("Transaksi");
+        transaksi.setBorder(null);
+        transaksi.setBorderPainted(false);
+        transaksi.setContentAreaFilled(false);
+        transaksi.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        transaksi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                transaksiActionPerformed(evt);
+            }
+        });
+        getContentPane().add(transaksi, new org.netbeans.lib.awtextra.AbsoluteConstraints(1160, 8, -1, 40));
+
+        riwayat.setBackground(new java.awt.Color(228, 228, 223));
+        riwayat.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        riwayat.setForeground(new java.awt.Color(255, 255, 255));
+        riwayat.setText("Riwayat");
+        riwayat.setBorder(null);
+        riwayat.setBorderPainted(false);
+        riwayat.setContentAreaFilled(false);
+        riwayat.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        riwayat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                riwayatActionPerformed(evt);
+            }
+        });
+        getContentPane().add(riwayat, new org.netbeans.lib.awtextra.AbsoluteConstraints(1054, 8, 90, 40));
+
+        inventori.setBackground(new java.awt.Color(228, 228, 223));
+        inventori.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        inventori.setForeground(new java.awt.Color(0, 66, 37));
+        inventori.setText("Inventori");
+        inventori.setBorder(null);
+        inventori.setBorderPainted(false);
+        inventori.setContentAreaFilled(false);
+        inventori.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        inventori.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inventoriActionPerformed(evt);
+            }
+        });
+        getContentPane().add(inventori, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 8, 90, 40));
+
+        member.setBackground(new java.awt.Color(228, 228, 223));
+        member.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        member.setForeground(new java.awt.Color(0, 66, 37));
+        member.setText("Member");
+        member.setBorder(null);
+        member.setBorderPainted(false);
+        member.setContentAreaFilled(false);
+        member.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        member.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                memberActionPerformed(evt);
+            }
+        });
+        getContentPane().add(member, new org.netbeans.lib.awtextra.AbsoluteConstraints(954, 8, 90, 40));
+
+        jPanel1.setBackground(new java.awt.Color(255, 176, 0));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1050, 10, 100, 40));
+
+        beranda.setBackground(new java.awt.Color(228, 228, 223));
+        beranda.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        beranda.setForeground(new java.awt.Color(0, 66, 37));
+        beranda.setText("Beranda");
+        beranda.setBorder(null);
+        beranda.setBorderPainted(false);
+        beranda.setContentAreaFilled(false);
+        beranda.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        beranda.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                berandaActionPerformed(evt);
+            }
+        });
+        getContentPane().add(beranda, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 8, 90, 40));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void transaksiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_transaksiActionPerformed
+        // TODO add your handling code here:
+        dispose();
+        new transaksipage().setVisible(true);
+    }//GEN-LAST:event_transaksiActionPerformed
+
+    private void riwayatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_riwayatActionPerformed
+        // TODO add your handling code here:
+        dispose();
+        new riwayatpage().setVisible(true);
+    }//GEN-LAST:event_riwayatActionPerformed
+
+    private void inventoriActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inventoriActionPerformed
+        // TODO add your handling code here:
+        dispose();
+        new homepage().setVisible(true);
+    }//GEN-LAST:event_inventoriActionPerformed
+
+    private void memberActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_memberActionPerformed
+        // TODO add your handling code here:
+        dispose();
+        new memberpage().setVisible(true);
+    }//GEN-LAST:event_memberActionPerformed
+
+    private void berandaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_berandaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_berandaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +219,14 @@ public class riwayatpage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton beranda;
+    private javax.swing.JButton inventori;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JButton member;
+    private javax.swing.JLabel relasijy;
+    private javax.swing.JButton riwayat;
+    private javax.swing.JButton transaksi;
     // End of variables declaration//GEN-END:variables
 }
